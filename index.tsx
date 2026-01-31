265d4e63b6385eeba4c29eaabbe22cab85120b1e
huynhbasonltk <huynhbasonltk@gmail.com>
2026-01-31 13:03:52 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import.meta.env.VITE_SCRIPT_URL;
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
